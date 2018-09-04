0a0a54060118f10d67b03005d79699878eb8fdd7
Nikhil Saxena <saxenanickk@gmail.com>
2018-09-04 15:21:08 +0530

    Updates

--- App.js
+++ App.js
@@ -1,14 +1,14 @@
-import React, { Component } from "react"
-import { StyleSheet, Text, View, Dimensions } from "react-native"
-import { StackNavigator, createBottomTabNavigator } from "react-navigation"
-import Animation from "lottie-react-native"
-import LoginScreen from "./Screens/LoginScreen"
-import Explore from "./Screens/Explore"
-import Inbox from "./Screens/Inbox"
-import Trips from "./Screens/Trips"
-import Saved from "./Screens/Saved"
+import React, { Component } from "react";
+import { StyleSheet, Text, View, Dimensions } from "react-native";
+import { StackNavigator, createBottomTabNavigator } from "react-navigation";
+import Animation from "lottie-react-native";
+import LoginScreen from "./Screens/LoginScreen";
+import Explore from "./Screens/Explore";
+import Inbox from "./Screens/Inbox";
+import Trips from "./Screens/Trips";
+import Saved from "./Screens/Saved";
 
-const { height, width } = Dimensions.get("window")
+const { height, width } = Dimensions.get("window");
 
 export default class App extends Component {
 	render() {
@@ -30,40 +30,36 @@ export default class App extends Component {
 			// 		/>
 			// 	</View>
 			// </View>
-		)
+		);
 	}
 }
 
-const AppNavigator = StackNavigator({
-	LoginScreen: { screen: LoginScreen },
-})
-
-createBottomTabNavigator(
+const AppNavigator = createBottomTabNavigator(
 	{
-		Explore: {
-			screen: Explore,
-			navigationOptions: {
-				tabBarLabel: "EXPLORE",
-			},
-		},
 		Inbox: {
 			screen: Inbox,
 			navigationOptions: {
-				tabBarLabel: "INBOX",
-			},
+				tabBarLabel: "INBOX"
+			}
+		},
+		Explore: {
+			screen: Explore,
+			navigationOptions: {
+				tabBarLabel: "EXPLORE"
+			}
 		},
 		Trips: {
 			screen: Trips,
 			navigationOptions: {
-				tabBarLabel: "TRIPS",
-			},
+				tabBarLabel: "TRIPS"
+			}
 		},
 		Saved: {
 			screen: Saved,
 			navigationOptions: {
-				tabBarLabel: "SAVED",
-			},
-		},
+				tabBarLabel: "SAVED"
+			}
+		}
 	},
 	{
 		animationEnabled: true,
@@ -76,17 +72,64 @@ createBottomTabNavigator(
 				shadowOffset: { width: 5, height: 3 },
 				shadowColor: "black",
 				shadowOpacity: 0.5,
-				elevation: 5,
-			},
+				elevation: 5
+			}
+		}
+	}
+);
+// StackNavigator({
+// 	LoginScreen: { screen: LoginScreen },
+// })
+
+createBottomTabNavigator(
+	{
+		Explore: {
+			screen: Explore,
+			navigationOptions: {
+				tabBarLabel: "EXPLORE"
+			}
 		},
+		Inbox: {
+			screen: Inbox,
+			navigationOptions: {
+				tabBarLabel: "INBOX"
+			}
+		},
+		Trips: {
+			screen: Trips,
+			navigationOptions: {
+				tabBarLabel: "TRIPS"
+			}
+		},
+		Saved: {
+			screen: Saved,
+			navigationOptions: {
+				tabBarLabel: "SAVED"
+			}
+		}
 	},
-)
+	{
+		animationEnabled: true,
+		tabBarOptions: {
+			activeTintColor: "red",
+			inactiveTintColor: "grey",
+			style: {
+				backgroundColor: "white",
+				borderTopWidth: 0,
+				shadowOffset: { width: 5, height: 3 },
+				shadowColor: "black",
+				shadowOpacity: 0.5,
+				elevation: 5
+			}
+		}
+	}
+);
 
 const styles = StyleSheet.create({
 	container: {
 		flex: 1,
 		backgroundColor: "#ffffff",
 		justifyContent: "center",
-		alignItems: "center",
-	},
-})
+		alignItems: "center"
+	}
+});
--- Screens/Inbox.js
+++ Screens/Inbox.js
@@ -1,16 +1,114 @@
-import React from "react"
-import { View, Text } from "react-native"
+import React from "react";
+import { View, Text, Animated, TouchableOpacity } from "react-native";
 
 export default class Inbox extends React.Component {
 	static navigationOptions = {
-		header: null,
+		header: null
+	};
+
+	constructor() {
+		super();
+		this.clicked = true;
+		this.animatedValue = new Animated.Value(0);
 	}
 
+	upAnimation = () => {
+		this.clicked = false;
+		Animated.timing(this.animatedValue, {
+			toValue: 1,
+			duration: 300
+		}).start();
+	};
+
+	downAnimation = () => {
+		this.clicked = true;
+		Animated.timing(this.animatedValue, {
+			toValue: 0,
+			duration: 300
+		}).start();
+	};
+
 	render() {
+		const viewHeight = this.animatedValue.interpolate({
+			inputRange: [0, 1],
+			outputRange: [0.5, 0.7]
+		});
+		const firstFlex = this.animatedValue.interpolate({
+			inputRange: [0, 0.5, 1],
+			outputRange: [1, 0, 0]
+		});
+		const secondFlex = this.animatedValue.interpolate({
+			inputRange: [0, 0.5, 1],
+			outputRange: [0, 1, 1]
+		});
+		const firstOpacity = this.animatedValue.interpolate({
+			inputRange: [0, 1],
+			outputRange: [1, 0]
+		});
+		const secondOpacity = this.animatedValue.interpolate({
+			inputRange: [0, 1],
+			outputRange: [0, 1]
+		});
 		return (
-			<View>
-				<Text>Inbox</Text>
+			<View
+				style={{
+					flex: 1,
+					backgroundColor: "#dfdfdf",
+					justifyContent: "flex-end"
+				}}
+			>
+				<Animated.View
+					style={{
+						backgroundColor: "#ffffff",
+						elevation: 10,
+						flex: viewHeight,
+						bottom: 0
+					}}
+				>
+					<Animated.View
+						style={{
+							flex: firstFlex,
+							backgroundColor: "red",
+							opacity: firstOpacity,
+							justifyContent: "center",
+							alignItems: "center"
+						}}
+					>
+						<TouchableOpacity
+							style={{
+								flex: 0.2
+							}}
+							onPress={() => {
+								if (this.clicked) this.upAnimation();
+								else this.downAnimation();
+							}}
+						>
+							<Text>Click</Text>
+						</TouchableOpacity>
+					</Animated.View>
+					<Animated.View
+						style={{
+							flex: secondFlex,
+							backgroundColor: "green",
+							opacity: secondOpacity,
+							justifyContent: "center",
+							alignItems: "center"
+						}}
+					>
+						<TouchableOpacity
+							style={{
+								flex: 0.2
+							}}
+							onPress={() => {
+								if (this.clicked) this.upAnimation();
+								else this.downAnimation();
+							}}
+						>
+							<Text>Click</Text>
+						</TouchableOpacity>
+					</Animated.View>
+				</Animated.View>
 			</View>
-		)
+		);
 	}
 }
